f24875329f786a63c55a0aeac9a52202538d467e
James Clevenger <jeclevenger@gmail.com>
2014-12-16 20:11:49 -0600

    initial commit

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$(".button").click(function() {
+		var id = $(this).attr("id");
+		var span_id = "#span_" + id;
+		var div_id = "#div_" + id;
+		$(span_id).toggleClass('glyphicon-chevron-down glyphicon-chevron-right');
+		$(div_id).toggle("clip");
+	});
+	$("#1").trigger("click");
+	
+});
---
+++ js/plot.js
@@ -0,0 +1,237 @@
+$(document).ready(function() {
+	//svg dims and margin  
+		var margin = {top: 20, right: 20, bottom: 20, left: 20},
+			width = 600,
+			height = 2000;
+	//create svg
+		svg = d3.select("#timeline").append("svg").attr({
+			width: width,
+			height: height,
+			id: "svg"
+		});
+	//function for scale
+	plotnames = []
+		function startFunction() {
+			plotnames = []
+			setdate = ""
+			$('.tier').each(function (index) {
+				if ($(this).find(':first-child').hasClass("glyphicon-ok")) {                                   
+					if (this.id == 11) {
+						plotnames.push(1);
+						plotnames.push(2);
+						setdate = "15 March 2010"
+					} else if (this.id == 22) {
+						plotnames.push(3);
+						setdate = "15 March 2005"
+					} else if (this.id == 33) {
+						plotnames.push(4);                    
+						plotnames.push(5);
+						setdate = "15 March 2000"
+					} else if (this.id == 44) {
+						plotnames.push(6);
+						setdate = "15 March 1995"
+					} else if (this.id == 55) {
+						plotnames.push(7);
+						plotnames.push(8);
+						setdate = "15 March 1990"
+					} else {
+						
+					}
+				} else {
+					
+				}
+			});
+		};
+	startFunction();
+	//plotting
+		var axisPadding = 45;
+		var width = 50;
+		var counter = 0;
+		var colPadding = 10;
+		var $root = $('html, body');
+		
+		var events = d3.select("#svg")
+
+
+
+		events.append("svg:g")
+			.attr("id", "barchart")
+			
+
+		var vis = d3.select("#barchart")
+
+	//scale
+		var timeScale = d3.time.scale()
+			.domain([new Date(setdate), new Date])
+			.nice(d3.time.year)
+			.rangeRound([height - margin.bottom, margin.top]);
+	//axis
+		var yAxis = d3.svg.axis().scale(timeScale).orient("left")
+			.ticks(d3.time.years, 1);
+
+		var yAxisGroup = svg.append("g").attr({
+			"class": "axis",
+			transform: "translate(" + [axisPadding, 0] + ")"
+			}).call(yAxis);
+		
+	function plot(data) {
+		timeScale.domain([new Date(setdate), new Date]).nice(d3.time.year).rangeRound([height - margin.bottom, margin.top]);
+		yAxisGroup.transition().ease("linear").duration(500).call(yAxis);
+		var eventAttrs = {
+				x: function (d, i) {
+					return width * (d.set - 1) + colPadding * d.set + axisPadding
+				},
+				y: function (d, i) {
+					if (d.end == "Current") {
+						return timeScale(new Date)
+					} else {
+						return timeScale(new Date(d.end))
+					}
+				},
+				width: width,
+				height: function (d, i) {
+						if (d.end == "Current") {
+							return (timeScale(new Date(d.start)) - timeScale(new Date)) 
+						} else {
+							return (timeScale(new Date(d.start)) - timeScale(new Date(d.end)))
+						}
+				},
+				fill: function (d, i) {
+					if (d.title == "Minnesota Public School") {
+						return "#c6dbef"
+					} else if (d.title == "Minnesota Private School") {
+						return "#9ecae1"
+					} else if (d.title == "Michigan Public School") {
+						return "#6baed6"
+					} else if (d.title == "Michigan Public University") {
+						return "#3182bd"
+					} else if (d.title == "Cumbria University (United Kingdom)") {
+						return "#08519c"
+					} else if (d.title == "Parental Home Teachings") {
+						return "#bae4b3"
+					} else if (d.title == "Indirect Computer Learning") {
+						return "#74c476"
+					} else if (d.title == "Direct Computer Learning") {
+						return "#238b45"
+					} else if (d.title == "Family Owned Resturant & Resort") {
+						return "#d4b9da"
+					} else if (d.title == "Super Market") {
+						return "#c994c7"
+					} else if (d.title == "Hardware Store") {
+						return "#df65b0"
+					} else if (d.title == "Self Employed Odd Jobs") {
+						return "#e7298a"
+					} else if (d.title == "Apartment Maintence") {
+						return "#ce1256"
+					} else if (d.title == "University Tutor") {
+						return "#91003f"
+					} else if (d.title == "R&D Machine Shop") {
+						return "#fc9272"
+					} else if (d.title == "Industry Leading Engine Manufacturer (R&D)") {
+						return "#de2d26"
+					} else if (d.title == "Casl Media LLC") {
+						return "#000"
+					} else {
+						return "#000"
+					}
+				}		
+		};
+
+	//previous
+		var rects = vis.selectAll("rect.bar")
+			.data(data)
+
+			//enter
+			rects.enter()
+				.append("svg:rect")
+				.attr("class", "bar")
+				.attr(eventAttrs)
+				.on("click", function (d, i) {
+					$("h5#timelineTitle").text(d.title);
+					$("h5#timelineTitle").css('color', $(this).css('fill'));
+					$("p#timelineDes").text(d.description);
+					d3.selectAll("rect").classed("selected", false);
+					d3.select(this).classed("selected", true);
+					$root.animate({
+						scrollTop: $( "h3#3" ).offset().top
+					}, 500);
+					return false;
+
+				});
+
+			//exit
+			rects.exit()
+				.transition()
+				.ease("linear")
+					.attr("height", 0)
+					.duration(500)
+					.remove()
+
+			//update
+			rects
+				.data(data)
+				.transition()
+				.ease("linear")
+					.duration(500)
+				.attr(eventAttrs)
+
+	};
+
+	function findSet(){
+		var inputdata = [];
+		plotnames.forEach(function(entry) {
+			 setcut = $.grep(info, function(n, i){
+				return n.set == entry
+			});
+			inputdata = inputdata.concat(setcut)
+		});
+		return inputdata
+	};
+
+
+	$(".tier").click(function() {
+		$( "#tier_" + this.id + "_span" ).toggleClass('glyphicon-ok glyphicon-remove');  
+
+		startFunction();
+		if (jQuery.isEmptyObject(plotnames)) {
+			
+		} else {
+			plot(findSet(info))
+		}
+
+	});
+	plot(findSet(info));
+	var unique = [];
+	for (i = 0; i < info.length; ++i) {
+		var test = $.inArray(info[i].title, unique);
+		if (test == -1) {
+		
+			var start = info[i].start;
+			var end = info[i].end;
+			
+			if (info[i].end == "Current") {
+				end = info[i].end;
+				start = start.slice(3);
+			} else {
+				start = start.slice(3);
+				end = end.slice(3);
+			}
+			
+			if (info[i].type == "employment") {
+				$("#employment").after("<p>" + info[i].description + "</p>");
+				$("#employment").after("<h5>" + info[i].title + " <small>" + start + " - " + end + "</small></h5>");
+			} else if (info[i].type == "formal education") {
+				$("#formal").after("<p>" + info[i].description + "</p>");
+				$("#formal").after("<h5>" + info[i].title + " <small>" + start + " - " + end + "</small></h5>");
+			} else if (info[i].type == "informal education") {
+				$("#informal").after("<p>" + info[i].description + "</p>");
+				$("#informal").after("<h5>" + info[i].title + " <small>" + start + " - " + end + "</small></h5>");
+			} else {
+			
+			}
+			
+			unique.push(info[i].title)
+		}
+		
+	}
+});
